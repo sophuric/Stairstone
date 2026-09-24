ffb29a390a0de38fa3480811312b83a514fdd906
sophur <48314599+sophuric@users.noreply.github.com>
2026-09-24 21:04:38 +1000

    use block tags

--- src/main/java/me/sophur/stairstone/StairstoneMain.java
+++ src/main/java/me/sophur/stairstone/StairstoneMain.java
@@ -4,7 +4,9 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
@@ -18,18 +20,16 @@ public final class StairstoneMain implements ModInitializer {
     public static final String MOD_ID_LOWER = "stairstone";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final TagKey<Block> REDSTONE_BLOCKING_DIRECTIONAL = TagKey.create(Registries.BLOCK, identifier("redstone_blocking_directional"));
+
     public static boolean getCanConnect(Direction upwardDirection, BlockGetter level, BlockPos separatingBlockPos, BlockState separatingBlockState) {
         if (upwardDirection.getAxis() == Direction.Axis.Y) return true;
+        if (!separatingBlockState.is(REDSTONE_BLOCKING_DIRECTIONAL)) return true;
 
-        Block separatingBlock = separatingBlockState.getBlock();
-
-        if (separatingBlock instanceof StairBlock || separatingBlock instanceof SlabBlock) {
-            // block connection if bottom face OR side face is solid
-            boolean blockedBottom = separatingBlockState.isFaceSturdy(level, separatingBlockPos, Direction.DOWN, SupportType.FULL),
-                    blockedSide = separatingBlockState.isFaceSturdy(level, separatingBlockPos, upwardDirection, SupportType.FULL);
-            if (blockedBottom || blockedSide) return false;
-        }
-        return true;
+        // block connection if bottom face OR side face is solid
+        boolean blockedBottom = separatingBlockState.isFaceSturdy(level, separatingBlockPos, Direction.DOWN, SupportType.FULL),
+                blockedSide = separatingBlockState.isFaceSturdy(level, separatingBlockPos, upwardDirection, SupportType.FULL);
+        return !blockedBottom && !blockedSide;
     }
 
     @Override
